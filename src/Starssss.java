274f0db29256b8917dfce41ede2aae4da2305aa8
Nancy Guerrero <league@Mac-mini-2.local>
2016-03-16 18:53:09 -0700

    3/16/16

---
+++ src/Starssss.java
@@ -0,0 +1,34 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class Starssss {
+
+	public static void main(String[] args) {
+	Robot star = new Robot("mini");
+	
+	for (int i = 0; i < 6; i++) {
+	star.penDown();
+	star.move(50);
+	star.turn(145);
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+}
